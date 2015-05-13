2b72cd9373812a5ed9512f8aaf4f30812ce9a5ca
tzuria <tzuria@gmail.com>
2015-05-13 15:24:11 +0300

    "GREEN: Given a name, check if the map includes a location information"

--- Crowdmap.py
+++ Crowdmap.py
@@ -8,4 +8,13 @@ class Crowdmap(object):
 	def is_location_for_name(self,name):
 		posts = [post for post in self.list if post.find(name) != -1  and post.find("at") != -1]
 		return len(posts) != 0
+		
+	def is_map_consistencies(self,name):
+		foundOne = False
+		for i in self.list:
+			if i.find(name) != -1 and foundOne:
+				return False
+			if i.find(name):
+				foundOne = True
 	
+		return True
--- FindAPersonTests.py
+++ FindAPersonTests.py
@@ -15,6 +15,11 @@ class FindAPersonTests(unittest.TestCase):
 	def test_existingLocationInformationReturnTrue(self):
 		location_exist = self.crowdmap.is_location_for_name("Or")
 		self.assertTrue(location_exist)
+		
+	def test_mapinConsistencies(self):
+		consistence = self.crowdmap.is_map_consistencies("Or")
+		self.assertFalse(consistence)
+		
 			
 if __name__ == '__main__':
 	unittest.main()
